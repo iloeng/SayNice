7b640e05045e4fad547aa037708213d6b6939994
excing <courage.exc@gmail.com>
2020-06-30 19:09:01 +0800

    更新默认推送目录

--- xyz.saynice/setup.sh
+++ xyz.saynice/setup.sh
@@ -4,8 +4,8 @@ work_path=$(dirname $(readlink -f $0))
 systemctl stop saynice-api
 systemctl stop saynice-web
 
-mv $work_path/saynice-api-amd64-linux-v1 /usr/bin/saynice-api
-mv $work_path/saynice-web-amd64-linux /usr/bin/saynice-web
+mv $work_path/saynice-api-amd64-linux-v1 /usr/local/bin/saynice-api
+mv $work_path/saynice-web-amd64-linux /usr/local/bin/saynice-web
 mv $work_path/saynice-api.service /lib/systemd/system
 mv $work_path/saynice-web.service /lib/systemd/system
 
