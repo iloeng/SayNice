4a9237f19713965e0d12f3c4a9f57d5b1cbad3a4
excing <courage.exc@gmail.com>
2020-07-04 17:14:42 +0800

    整理 http 请求函数。

--- source/web/res/static/saynice.js
+++ source/web/res/static/saynice.js
@@ -15,6 +15,16 @@ var themes = [
   "accent black",
 ]
 
+function initTheme() {
+  var theme = Cookies.get("theme")
+
+  if (theme) {
+    setTheme(theme)
+  } else {
+    randTheme()
+  }
+}
+
 function randTheme() {
   var i = Math.random() * 10000 % themes.length
   var x = themes[Math.floor(i)]
@@ -29,10 +39,31 @@ function setTheme(theme) {
   Cookies.set("theme", theme)
 }
 
-var theme = Cookies.get("theme")
+initTheme()
 
-if (theme) {
-  setTheme(theme)
-} else {
-  randTheme()
+function httpGet(url, callback) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.responseType = "json";
+  request.send();
+  request.onload = function () {
+    if (200 != request.status) {
+      Snackbar.show({ text: "网络异常，请稍后再试。", });
+    } else {
+      callback(request.response)
+    }
+  }
+}
+
+function httpPost(url, data, callback) {
+  var request = new XMLHttpRequest();
+  request.open("POST", url);
+  request.send(JSON.stringify(data));
+  request.onload = function () {
+    if (200 != request.status) {
+      Snackbar.show({ text: "网络异常，请稍后再试。", });
+    } else {
+      callback(request.response)
+    }
+  }
 }
